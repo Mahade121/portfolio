f15626e2a90a524d6705da40dcc9b7fd5bd4397d
mahade <mahade.eftworkbd@gmail.com>
2023-01-15 16:53:30 +0600

    hero section created

--- src/pages/home.js
+++ src/pages/home.js
@@ -18,26 +18,24 @@ export default function home() {
                         <li>News</li>
                         <li>Contact</li>
                         <li className='dropdown'><span className='more'>More</span> <IconContext.Provider
-value={{ color: 'black', size: '17px', marginTop: '5px' }}>
+                            value={{ color: 'black', size: '17px', marginTop: '5px' }}>
                             <div className='down-icon'>
                                 <BsChevronDown />
                             </div>
                         </IconContext.Provider></li>
                     </ul>
                 </nav>
-                {/* <IconContext.Provider
-                    value={{ color: 'black', size: '22px' }}
-                >
-                    <div>
-                        <FiSearch />
-                    </div>
-                </IconContext.Provider> */}
                 <div className='search-input'>
                     <input type="text" placeholder='search here...' className='search' />
                 </div>
             </header>
             <main>
-
+                <div className="hero-sec">
+                    <h1 className='hero-title'>Hey! I Am <br /> Arfius Al-din</h1>
+                    <p className='bio'>I am the founder and ceo of <span className='website-link'>e-freelancing.com</span> <br /> which is the oldest freelancing website on the internet. So come here and learn to earn.</p>
+                    <button className='know-btn'>Know More</button>
+                    <button className='blog-btn'>Read Blogs</button>
+                </div>
             </main>
         </>
     )
