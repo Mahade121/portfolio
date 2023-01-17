639e6ea8e52b25ed2dddc7922efac6d9d067f94f
mahade <mahade.eftworkbd@gmail.com>
2023-01-17 13:01:52 +0600

    particle added

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,102 @@
 import { IconContext } from 'react-icons';
-import { FiSearch } from 'react-icons/fi';
 import { BsChevronDown } from 'react-icons/bs';
 import Typewriter from 'typewriter-effect';
 import Image from 'next/image';
 
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
 export default function home() {
+    const particlesInit = useCallback(async engine => {
+        console.log(engine);
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async container => {
+        await console.log(container);
+    }, []);
     return (
         <>
-            <header>
+        <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+                background: {
+                    color: {
+                        value: "#0d47a1",
+                    },
+                },
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: true,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#ffffff",
+                    },
+                    links: {
+                        color: "#ffffff",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    collisions: {
+                        enable: true,
+                    },
+                    move: {
+                        directions: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 2,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 5 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
+        <div className="absolute">
+        <header>
                 <div className="logo">
                     <h1>Arfius Al-din</h1>
                 </div>
@@ -67,6 +156,7 @@ export default function home() {
                     <h1>hello world</h1>
                     <h1>hello world</h1>
             </main>
+        </div>
         </>
     )
 }
