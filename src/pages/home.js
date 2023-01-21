0762c9b4036a82a896d1585b37471b2fc719fcd8
mahade <mahade.eftworkbd@gmail.com>
2023-01-21 15:49:31 +0600

    popup section added

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,17 @@
 import { IconContext } from 'react-icons';
 import { BsChevronDown } from 'react-icons/bs';
+import { BiUser } from 'react-icons/bi';
+import { AiOutlineTrophy } from 'react-icons/ai';
 import Typewriter from 'typewriter-effect';
 import Image from 'next/image';
 import Head from 'next/head';
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function home() {
+    const [btn1, setBtn1] = useState(false);
+    const [btn2, setBtn2] = useState(false);
+    const [btn3, setBtn3] = useState(false);
 
     return (
         <>
@@ -26,7 +32,7 @@ export default function home() {
                         <li>News</li>
                         <li>Contact</li>
                         <li className='dropdown'><span className='more'>More</span> <IconContext.Provider
-                            value={{ color: 'black', size: '17px', marginTop: '5px' }}>
+                            value={{ color: 'white', size: '17px', marginTop: '5px' }}>
                             <div className='down-icon'>
                                 <BsChevronDown />
                             </div>
@@ -60,21 +66,21 @@ export default function home() {
 
 
                 <div className="latest-blog">
-                    <h1 className='latest-blog-title'>Latest Blog</h1>
+                    <h1 className='sec-title'>Latest Blog</h1>
                     <div class="latest-blog-container">
                         <div class="card">
                             <div class="card__header">
                                 <Image src="/blockchain-blog.jpeg" alt="card__image" class="card__image" width="1080" height="250" />
                             </div>
                             <div class="card__body">
-                                {/* <span class="tag tag-blue">Technology</span> */}
+                                {/* <span class="tag tag-purple">Technology</span> */}
                                 <h5 className='card-date'>January 15, 2023</h5>
                                 <h4 className='card-title'>What's new in 2022 Tech</h4>
                                 <p className='card-detail'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi... <span><Link className='readmore-btn' href={'https://www.labnol.org/'}>Read More</Link></span> </p>
                                 <div className="tag-footer">
-                                <span class="tag tag-blue">Technology</span>
-                                <span class="tag tag-red">Technology</span>
-                                <span class="tag tag-brown">Technology</span>
+                                    <span class="tag tag-purple">Technology</span>
+                                    <span class="tag tag-green">Technology</span>
+                                    <span class="tag tag-yellow">Technology</span>
                                 </div>
                             </div>
                         </div>
@@ -83,14 +89,14 @@ export default function home() {
                                 <Image src="/thumbnail-thanks.jpg" alt="card__image" class="card__image" width="1080" height="250" />
                             </div>
                             <div class="card__body">
-                                {/* <span class="tag tag-blue">Technology</span> */}
+                                {/* <span class="tag tag-purple">Technology</span> */}
                                 <h5 className='card-date'>January 15, 2023</h5>
                                 <h4 className='card-title'>What's new in 2022 Tech</h4>
                                 <p className='card-detail'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi... <span><Link className='readmore-btn' href={'https://www.labnol.org/'}>Read More</Link></span> </p>
                                 <div className="tag-footer">
-                                <span class="tag tag-red">Technology</span>
-                                <span class="tag tag-brown">Technology</span>
-                                <span class="tag tag-blue">Technology</span>
+                                    <span class="tag tag-green">Technology</span>
+                                    <span class="tag tag-yellow">Technology</span>
+                                    <span class="tag tag-purple">Technology</span>
                                 </div>
                             </div>
                         </div>
@@ -99,18 +105,93 @@ export default function home() {
                                 <Image src="/thumbnail-design.jpg" alt="card__image" class="card__image" width="1080" height="250" />
                             </div>
                             <div class="card__body">
-                                {/* <span class="tag tag-blue">Technology</span> */}
+                                {/* <span class="tag tag-purple">Technology</span> */}
                                 <h5 className='card-date'>January 15, 2023</h5>
                                 <h4 className='card-title'>What's new in 2022 Tech</h4>
                                 <p className='card-detail'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi... <span><Link className='readmore-btn' href={'https://www.labnol.org/'}>Read More</Link></span> </p>
                                 <div className="tag-footer">
-                                <span class="tag tag-blue">Technology</span>
-                                <span class="tag tag-brown">Technology</span>
-                                <span class="tag tag-red">Technology</span>
+                                    <span class="tag tag-purple">Technology</span>
+                                    <span class="tag tag-yellow">Technology</span>
+                                    <span class="tag tag-green">Technology</span>
                                 </div>
                             </div>
                         </div>
                     </div>
+                </div>
+
+
+                <div className="dreams">
+                    <h1>Dreams To Reality</h1>
+                    <p className='dreams-p'> আপনি এখন কোন ধাপে আটকে আছেন? </p>
+                    <IconContext.Provider
+                        value={{ color: 'white', size: '70px', marginTop: '5px' }}>
+                        <div>
+                            <BiUser /><br />
+                        </div>
+                    </IconContext.Provider>
+
+                    <Image src="/arrow.png" alt="arrow" className="arrow" width="36" height="130" /><br />
+                    <button className='dream-btn' onClick={() => setBtn1(!btn1)}>Dream</button><br />
+                    {btn1 && (
+                        <>
+                            <br /><div className="popup-box transform">
+                                <div className="popupbox-content">
+                                    <h4 className='popupbox-title'>Dream</h4>
+                                    <div className="popupbox-btn">
+                                        <button>Demo</button>
+                                        <button>Demo</button>
+                                        <button>Demo</button>
+                                        <button>Demo</button>
+                                        <button>Demo</button>
+                                        <button>Demo</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    )}
+
+                    <Image src="/arrow.png" alt="arrow" className="arrow" width="36" height="130" /><br />
+                    <button className='dream-btn' onClick={() => setBtn2(!btn2)}>Goal</button><br />
+                    {btn2 && (
+                        <>
+                            <br /><div className="popup-box transform">
+                                <h4 className='popupbox-title'>Goal</h4>
+                                <div className="popupbox-btn">
+                                    <button>Demo</button>
+                                    <button>Demo</button>
+                                    <button>Demo</button>
+                                    <button>Demo</button>
+                                    <button>Demo</button>
+                                    <button>Demo</button>
+                                </div>
+                            </div>
+                        </>
+                    )}
+
+                    <Image src="/arrow.png" alt="arrow" className="arrow" width="36" height="130" /><br />
+                    <button className='dream-btn' onClick={() => setBtn3(!btn3)}>Plan</button><br />
+                    {btn3 && (
+                        <>
+                            <br /><div className="popup-box transform">
+                                <h4 className='popupbox-title'>Plan</h4>
+                                <div className="popupbox-btn">
+                                    <button>Demo</button>
+                                    <button>Demo</button>
+                                    <button>Demo</button>
+                                    <button>Demo</button>
+                                    <button>Demo</button>
+                                    <button>Demo</button>
+                                </div>
+                            </div>
+                        </>
+                    )}
+                    <Image src="/arrow.png" alt="arrow" className="arrow" width="36" height="130" /><br />
+                    <IconContext.Provider
+                        value={{ color: 'white', size: '70px', marginTop: '20px' }}>
+                        <div>
+                            <br /><AiOutlineTrophy /><br />
+                        </div>
+                    </IconContext.Provider>
                 </div>
 
 
---
+++ src/pages/news.js
@@ -0,0 +1,7 @@
+export default function home() {
+    return (
+        <>
+            <h1>All News & Updates</h1>
+        </>
+    )
+}
