e2df3e958c7493883cd5416b1bb7d0a1e62028f1
mahade <mahade.eftworkbd@gmail.com>
2023-01-27 11:08:17 +0600

    commit from laptop

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,7 +1,30 @@
 export default function home() {
     return (
-        <>
+        <div className="news-section">
             <h1>All News & Updates</h1>
-        </>
+            <h1>hello world</h1>
+            <h1>hello world</h1>
+            <h1>hello world</h1>
+            <h1>hello world</h1>
+            <h1>hello world</h1>
+            <h1>hello world</h1>
+            <h1>hello world</h1>
+            <h1>hello world</h1>
+            <h1>hello world</h1>
+            <h1>hello world</h1>
+            <h1>hello world</h1>
+            <h1>hello world</h1>
+            <h1>hello world</h1>
+            <h1>hello world</h1>
+            <h1>hello world</h1>
+            <h1>hello world</h1>
+            <h1>hello world</h1>
+            <h1>hello world</h1><h1>hello world</h1>
+            <h1>hello world</h1>
+            <h1>hello world</h1>
+            <h1>hello world</h1>
+            <h1>hello world</h1>
+            <h1>hello world</h1>
+        </div>
     )
 }
