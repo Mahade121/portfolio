bb7875f67baa8b766499d5c952cedaab4e8dd106
mahade <mahade.eftworkbd@gmail.com>
2023-01-28 16:22:27 +0600

    all navigation created, notes section created, stories content added

--- src/pages/home.js
+++ src/pages/home.js
@@ -119,12 +119,12 @@ export default function home() {
                 </div>
                 <nav>
                     <ul>
-                        <li>Home</li>
-                        <li>Stories</li>
-                        <li>Notes</li>
-                        <li>Books</li>
-                        <li>News</li>
-                        <li>Contact</li>
+                        <li><Link className='nav-link' href={'/home'}>Home</Link></li>
+                        <li><Link className='nav-link' href={'/stories'}>Stories</Link></li>
+                        <li><Link className='nav-link' href={'/Notes'}>Notes</Link></li>
+                        <li><Link className='nav-link' href={'/Books'}>Books</Link></li>
+                        <li><Link className='nav-link' href={'/news'}>News</Link></li>
+                        <li><Link className='nav-link' href={'/contact'}>Contact</Link></li>
                         <li className='dropdown'><span className='more'>More</span> <IconContext.Provider
                             value={{ color: 'white', size: '17px', marginTop: '5px' }}>
                             <div className='down-icon'>
@@ -416,6 +416,57 @@ export default function home() {
                                 </div>
                             </div>
                         </div><br /><br /><br />
+                    </div>
+                    <div className="view-more-btn-container">
+                        <button class="view-more-btn" role="button">View More</button>
+                    </div>
+                </div>
+
+                <div className="latest-blog">
+                    <h1 className='sec-title'>Latest Notes</h1>
+                    <div class="latest-notes-container">
+                        <div class="notes-card">
+                            <div class="card__header">
+                                <Image src="/blockchain-blog.jpeg" alt="card__image" class="card__image" width="1080" height="250" />
+                            </div>
+                            <div class="card__body">
+                                <h4 className='card-title'>What's new in 2022 Tech</h4>
+                                <p className='card-detail'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi... <span><Link className='readmore-btn' href={'https://www.labnol.org/'}>Read More</Link></span> </p>
+                                <div className="tag-footer">
+                                    <span class="tag tag-purple">Technology</span>
+                                    <span class="tag tag-yellow">Technology</span>
+                                    <span class="tag tag-green">Technology</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="notes-card">
+                            <div class="card__header">
+                                <Image src="/thumbnail-thanks.jpg" alt="card__image" class="card__image" width="1080" height="250" />
+                            </div>
+                            <div class="card__body">
+                                <h4 className='card-title'>What's new in 2022 Tech</h4>
+                                <p className='card-detail'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi... <span><Link className='readmore-btn' href={'https://www.labnol.org/'}>Read More</Link></span> </p>
+                                <div className="tag-footer">
+                                    <span class="tag tag-purple">Technology</span>
+                                    <span class="tag tag-yellow">Technology</span>
+                                    <span class="tag tag-green">Technology</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="notes-card">
+                            <div class="card__header">
+                                <Image src="/thumbnail-design.jpg" alt="card__image" class="card__image" width="1080" height="250" />
+                            </div>
+                            <div class="card__body">
+                                <h4 className='card-title'>What's new in 2022 Tech</h4>
+                                <p className='card-detail'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi... <span><Link className='readmore-btn' href={'https://www.labnol.org/'}>Read More</Link></span> </p>
+                                <div className="tag-footer">
+                                    <span class="tag tag-purple">Technology</span>
+                                    <span class="tag tag-yellow">Technology</span>
+                                    <span class="tag tag-green">Technology</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div className="view-more-btn-container">
                         <button class="view-more-btn" role="button">View More</button>
---
+++ src/pages/stories.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { IconContext } from 'react-icons';
+import { BsChevronDown } from 'react-icons/bs';
+import { BiUser } from 'react-icons/bi';
+import { AiOutlineTrophy } from 'react-icons/ai';
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function stories() {
+    return (
+        <>
+            <div className="stories-section">
+                <header>
+                    <div className="logo">
+                        <h1>Arfius Al-din</h1>
+                    </div>
+                    <nav>
+                        <ul>
+                            <li><Link className='nav-link' href={'/home'}>Home</Link></li>
+                            <li><Link className='nav-link' href={'/stories'}>Stories</Link></li>
+                            <li><Link className='nav-link' href={'/notes'}>Notes</Link></li>
+                            <li><Link className='nav-link' href={'/books'}>Books</Link></li>
+                            <li><Link className='nav-link' href={'/news'}>News</Link></li>
+                            <li><Link className='nav-link' href={'/contact'}>Contact</Link></li>
+                            <li className='dropdown'><span className='more'>More</span> <IconContext.Provider
+                                value={{ color: 'white', size: '17px', marginTop: '5px' }}>
+                                <div className='down-icon'>
+                                    <BsChevronDown />
+                                </div>
+                            </IconContext.Provider></li>
+                        </ul>
+                    </nav>
+                    <div className='search-input'>
+                        <input type="text" placeholder='search here...' className='search' />
+                    </div>
+                </header>
+                <div className="stories-container">
+                    <br /><br /><br />
+                    <div className="content-section">
+                        <h1 className='stories-title'>Stories</h1>
+                        <p>Arfius Al-din (Bengali: আর্ফিয়াস আল-দ্বীন; 28 December 1996), a Bangladeshi entrepreneur who is the founder of e-freelancing.com (Bengali: ই-ফ্রিল্যান্সিং ডট কম) which is the oldest freelancing website on the internet (ref. Whois). The company motto is “ #Learn_to_Earn.”
+                            Everyone tries to make a difference in their lives and help other people. That’s why it makes sense to become an entrepreneur, because not only do you get to establish a business growth, but you can help other people as well. That’s what Arfius Al-din did. He used his passion for IT and fashion to help many people from all over the world express themselves, find new ways to talk about the stuff they like, and truly push their lives to the next level.</p>
+                        <Image src="/arfiusaldin-news.jpg" alt="" width='512' height='320' />
+                        <p><b>Early life and studies</b><br />
+                            Arfius Al-din was born in Rajshahi, Bangladesh. Where he lived for six years. When he was a child, his passion was cricket and football, and he did want to become a cricket player. Destiny took him to a new place. He studied at the Dhanmondi Government Boys’ High School, acquiring a B.sc in electrical and electronic engineering. This was the time when Arfius Al-din started pursuing his passion for a career into IT Sector as an Entrepreneur.</p>
+                    </div>
+                    <div className="youtube-section">
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
